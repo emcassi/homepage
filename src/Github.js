bbb24c18f0e10a3e0c2dee4e5e1e8a5c081bf884
emcassi <alex.wayne.dev@gmail.com>
2022-11-02 22:15:35 -0700

    Initial commit, done

--- src/Github.js
+++ src/Github.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Github.css'
+import "./Github.css";
 
 function Github() {
   return (
@@ -7,7 +7,9 @@ function Github() {
       <div className="github-content">
         <img src="res/github.png" alt="" />
         <h1>View my work on GitHub</h1>
-        <button><a href="#">Visit</a></button>
+        <button>
+          <a href="https://github.com/emcassi">Visit</a>
+        </button>
       </div>
     </div>
   );
